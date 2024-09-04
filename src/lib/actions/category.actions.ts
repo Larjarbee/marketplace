84e175f34377333f99834dbf6fd0bb75349b6bce
osunlajajamiu97 <osunlajajamiu97@gmail.com>
2024-09-04 22:02:38 +0100

    fixes

--- src/lib/actions/category.actions.ts
+++ src/lib/actions/category.actions.ts
@@ -1,13 +1,10 @@
-'use server';
+"use server";
 
-import { CreateCategoryParams } from '@/types';
-import { handleError } from '../utils';
-import { connectToDatabase } from '../database';
-import Category from '../database/models/category.model';
+import { handleError } from "../utils";
+import { connectToDatabase } from "../database";
+import Category from "../database/models/category.model";
 
-export const createCategory = async ({
-  categoryName,
-}: CreateCategoryParams) => {
+export const createCategory = async (categoryName: string) => {
   try {
     await connectToDatabase();
 
