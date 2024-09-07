f67659151b79945f983e01d1206fc2c06f7d0807
osunlajajamiu97 <osunlajajamiu97@gmail.com>
2024-09-07 20:02:50 +0100

    fixes

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/shared/header";
 import Footer from "@/components/shared/footer";
+import { Suspense } from "react";
 
 export default function RootLayout({
   children,
@@ -7,10 +8,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
+    <Suspense>
       <Header />
       <main>{children}</main>
       <Footer />
-    </>
+    </Suspense>
   );
 }
